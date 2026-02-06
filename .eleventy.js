41ccb46bfe78635412bd4cb1c4ea7a86d6339107
whollysmoke9232-hue <whollysmoke9232@gmail.com>
2026-02-06 16:13:58 -0500

    Restore filters and rebuild site

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,18 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets");
   eleventyConfig.addPassthroughCopy("src/images");
 
+  eleventyConfig.addFilter("isoDate", function(dateObj) {
+    return new Date(dateObj).toISOString();
+  });
+
+  eleventyConfig.addFilter("readableDate", function(dateObj) {
+    return new Date(dateObj).toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "long",
+      day: "numeric"
+    });
+  });
+
   return {
     dir: {
       input: "src",
